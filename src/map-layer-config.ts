547215b0f58db39d684457a8a97f2094d32d75f8
Mac <nmacawile@gmail.com>
2018-11-21 15:11:07 +0800

    Expand zoom range

--- src/map-layer-config.ts
+++ src/map-layer-config.ts
@@ -1,8 +1,8 @@
 export const mapLayerConfig = {
   url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
   options: {
-    minZoom: 4,
-    maxZoom: 14,
+    minZoom: 3,
+    maxZoom: 16,
     attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
   }
 }
